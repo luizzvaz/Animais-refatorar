a60bfb966b9418d415349379e3680a8475327bc8
lipe <luizvaz.estudo@gmail.com>
2023-11-28 09:52:55 -0300

    scroll-suave
    transformando em classe

--- PROJETO-1 continuar/js1/module/suavescroll.js
+++ PROJETO-1 continuar/js1/module/suavescroll.js
@@ -1,29 +1,28 @@
-export default function intScrollSuave() {
-  const linksInternos = document.querySelectorAll('[data-scroll="suave"] a[href^="#"]');
-
-  function scrollSuave(event) {
+export default class ScrollSuave {
+  constructor(links, options) {
+    this.linksInternos = document.querySelectorAll(links);
+    if (options === undefined) {
+      this.options = { behavior: 'smooth', block: 'start' };
+    } else {
+      this.options = options;
+    }
+    this.scrollSuave2 = this.scrollSuave2.bind(this);
+  }
+  scrollSuave2(event) {
     event.preventDefault();
     const herfe = event.currentTarget.getAttribute('href');
     const secao = document.querySelector(herfe);
-    const topo = secao.offsetTop;
-    window.scrollTo({
-      top: topo,
-      behavior: 'smooth',
+    secao.scrollIntoView(this.options);
+  }
+  addLikEvent() {
+    this.linksInternos.forEach((link) => {
+      link.addEventListener('click', this.scrollSuave2);
     });
   }
-  if (linksInternos.length) {
-    // function scrollSuave2(event) {
-    //   event.preventDefault();
-    //   const herfe = event.currentTarget.getAttribute('href')
-    //   const secao = document.querySelector(herfe)
-    //   secao.scrollIntoView({
-    //     behavior: "smooth",
-    //     block: 'start',
-    //   })
-    //   //esse não precisa saber a distancia, apenas selecionar o elemento!
-    // }
-    linksInternos.forEach((link) => {
-      link.addEventListener('click', scrollSuave);
-    });
+  init() {
+    if (this.linksInternos.length) {
+      this.addLikEvent()
+    }
+    return this;
   }
-}
+};
--- PROJETO-1 continuar/js1/script.js
+++ PROJETO-1 continuar/js1/script.js
@@ -1,6 +1,6 @@
 import navtab from './module/navtab.js';
 import acordion from './module/acodionn.js';
-import suavesScroll from './module/suavescroll.js';
+import ScrollSuave from './module/suavescroll.js';
 import pageint from './module/animapage.js';
 import modal from './module/modalLongin.js';
 import balonmap from './module/balonmap.js';
@@ -9,10 +9,11 @@ import menuMobile from './module/menuMobile.js';
 import horario from './module/horarioFunc.js';
 import infofetch from './module/fetchanimais.js';
 
+const scrollsuave = new ScrollSuave('[data-scroll="suave"] a[href^="#"]')
+scrollsuave.init();
 horario();
 acordion();
 navtab();
-suavesScroll();
 pageint();
 modal();
 balonmap();
